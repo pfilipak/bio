31b0757861d6e00709c3de0da3b1c57df6706918
Paulo Andre <pfilipak@gmail.com>
2016-04-02 22:37:24 -0300

    arquivo de configuracao

--- app/controllers/FromClientController.java
+++ app/controllers/FromClientController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.io.File;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,23 +15,19 @@ import play.mvc.Http.Header;
 public class FromClientController extends Controller {
 
 	public static void formList(String body) {
-				Map<String, play.mvc.Http.Header> headers = response.headers;
+		Map<String, play.mvc.Http.Header> headers = response.headers;
 		headers.put("contentType", new Header("contentType", "text/xml"));
 		headers.put("charset", new Header("charset", "utf8"));
 		headers.put("X-OpenRosa-Version", new Header("X-OpenRosa-Version", "1.0"));
 		Logger.info(readFileAsList());
 		renderXml(readFileAsList());
-//		renderXml("<xforms>FormBorboleta.xml;asfd;asfd;asfd</xforms>");
-		//public/images/favicon.png
 	}
 	
 	private static String readFileAsList() {
         String fileName = "/Users/pfilipak/Downloads/javaRosa.xml";
-//        String fileName = "/Users/pfilipak/Downloads/FormBorboleta.xml";
 
         StringBuilder content = new StringBuilder();
         try {
-//            URI uri = FromClientController.class.getResource(fileName).toURI();
             List<String> lines = Files.readAllLines(Paths.get(fileName),
                     Charset.defaultCharset());
             for (String line : lines) {
@@ -40,7 +38,18 @@ public class FromClientController extends Controller {
         }
         return content.toString();
     }
-	public static void submission(String body) {
-		Logger.info(body);
+	public static void submission(String body, File attachment) {
+		Logger.info("submition[%s] [%s]", body, attachment);
+		Map<String, play.mvc.Http.Header> headers = response.headers;
+		headers.put("Location", new Header("Location", URLEncoder.encode("http://192.168.0.100:9000/accept")));
+//		headers.put("contentType", new Header("contentType", "text/xml"));
+//		headers.put("charset", new Header("charset", "utf8"));
+//		headers.put("X-OpenRosa-Version", new Header("X-OpenRosa-Version", "1.0"));
+		response.status = 204;
+	}
+	
+	public static void accept(String body, File attachment){
+		Logger.info("submition[%s] [%s]", body, attachment);
+		response.status = 201;
 	}
 }
