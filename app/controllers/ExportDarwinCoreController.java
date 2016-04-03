ab59f03c443b51bfeb78a84bdecde08bb57e7c9f
Paulo Andre <pfilipak@gmail.com>
2016-04-03 08:38:54 -0300

    incluir view para exportar darwincore

--- app/controllers/ExportDarwinCoreController.java
+++ app/controllers/ExportDarwinCoreController.java
@@ -1,5 +1,14 @@
 package controllers;
 
-public class ExportDarwinCoreController {
+import java.util.List;
 
+import models.FileUpload;
+import play.mvc.Controller;
+
+public class ExportDarwinCoreController extends Controller {
+
+	public static void index(){
+		List<FileUpload> fileList = FileUpload.findAll();
+		render(fileList);
+	}
 }
