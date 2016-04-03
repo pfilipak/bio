047a68b66138d4fbb5905f3dccb0b99ca33f77bf
Paulo Andre <pfilipak@gmail.com>
2016-04-03 14:22:03 -0300

    edicao de ObservacaoDetail

--- app/controllers/ObservacaoDetails.java
+++ app/controllers/ObservacaoDetails.java
@@ -1,8 +1,35 @@
 package controllers;
 
+import play.data.binding.Binder;
+import play.db.Model;
+import play.exceptions.TemplateNotFoundException;
+import controllers.CRUD.ObjectType;
+import models.Observacao;
 import models.ObservacaoDetail;
 
 @CRUD.For(ObservacaoDetail.class)
 public class ObservacaoDetails extends CRUD {
 
+	public static void save(String id) throws Exception {
+        ObjectType type = ObjectType.get(getControllerClass());
+        notFoundIfNull(type);
+        Model object = type.findById(id);
+        notFoundIfNull(object);
+        Binder.bindBean(params.getRootParamNode(), "object", object);
+        validation.valid(object);
+        if (validation.hasErrors()) {
+            renderArgs.put("error", play.i18n.Messages.get("crud.hasErrors"));
+            try {
+                render(request.controller.replace(".", "/") + "/show.html", type, object);
+            } catch (TemplateNotFoundException e) {
+                render("CRUD/show.html", type, object);
+            }
+        }
+        object._save();
+        flash.success(play.i18n.Messages.get("crud.saved", type.modelName));
+        if (params.get("_save") != null) {
+            redirect("/upload/list/" + ((ObservacaoDetail)object).observacao.fileUpload.id);
+        }
+        redirect(request.controller + ".show", object._key());
+    }
 }
--- app/controllers/Observacaos.java
+++ app/controllers/Observacaos.java
@@ -3,12 +3,38 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import controllers.CRUD.ObjectType;
 import models.Observacao;
 import play.Logger;
+import play.data.binding.Binder;
 import play.db.Model;
+import play.exceptions.TemplateNotFoundException;
 
 @CRUD.For(Observacao.class)
 public class Observacaos extends CRUD {
+
+	 public static void save(String id) throws Exception {
+	        ObjectType type = ObjectType.get(getControllerClass());
+	        notFoundIfNull(type);
+	        Model object = type.findById(id);
+	        notFoundIfNull(object);
+	        Binder.bindBean(params.getRootParamNode(), "object", object);
+	        validation.valid(object);
+	        if (validation.hasErrors()) {
+	            renderArgs.put("error", play.i18n.Messages.get("crud.hasErrors"));
+	            try {
+	                render(request.controller.replace(".", "/") + "/show.html", type, object);
+	            } catch (TemplateNotFoundException e) {
+	                render("CRUD/show.html", type, object);
+	            }
+	        }
+	        object._save();
+	        flash.success(play.i18n.Messages.get("crud.saved", type.modelName));
+	        if (params.get("_save") != null) {
+	            redirect("/upload/list/" + ((Observacao)object).fileUpload.id);
+	        }
+	        redirect(request.controller + ".show", object._key());
+	    }
 
 	 public static void list(int page, String search, String searchFields, String orderBy, String order) {
         ObjectType type = ObjectType.get(getControllerClass());
